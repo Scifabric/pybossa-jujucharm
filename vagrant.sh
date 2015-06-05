26d7f5f6c583a740571e5c55028946e00b1c9066
Marvin Reimer <therealmarv@gmail.com>
2015-06-05 14:44:46 +0200

    show juju gui password and fix install for shell

--- vagrant.sh
+++ vagrant.sh
@@ -5,3 +5,9 @@ set -ex
 apt-get update -y
 apt-get install -y git-core python3-pip libyaml-cpp0.3-dev
 pip3 install juju-git-deploy
+
+echo
+echo Ubuntu Juju GUI password
+grep admin-secret: /home/vagrant/.juju/environments.yaml
+
+echo "Finished Vagrant install!"
